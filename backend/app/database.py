9ff89f284b9934f0014db8ee485ef0bbd9ab4bd2
Your Name <youremail@yourdomain.com>
2025-08-11 14:18:09 +0800

    M

--- backend/app/database.py
+++
@@ -1,8 +0,0 @@
-# 数据库连接配置
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://user:password@localhost:5432/imagedb"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
